907cceb60602afa1a1a02e7a20363ef659ed33e7
coke_bdj <30370409+cokebdj@users.noreply.github.com>
2024-10-09 21:01:39 +0200

    fix sizes

--- 03_entrega/pinterestAsync/src/components/GalleryItem/GalleryItem.js
+++ 03_entrega/pinterestAsync/src/components/GalleryItem/GalleryItem.js
@@ -2,21 +2,25 @@ import './GalleryItem.css'
 
 export const cardTemplate = (item) => {
   return `
-    <li class="gallery-item" style="background-image: url(${item.urls.regular}); border: 10px solid ${item.color}">
+    <li class="gallery-item" style="background-image: url(${
+      item.urls.regular
+    });">
     <div class="info">
-        <div class="save-btn">
-            <button>Guardar</button>
+        <div class="icons">
+            <span class="photos-icon">📷 ${item.user.total_photos}</span>
+            <span class="likes-icon">❤️ ${item.likes}</span>
         </div>
-        <div class="links">
-            <a href=${item.links.html} class="full-link">${item.links.html}</a>
-            <div>
-                <a href=${item.urls.full} target="_blank" class="links-icon">
-                    <img src="/icons/upload.svg" alt="Upload icon"/>
-                </a>
-                <a href="#null" class="user-icon">
-                <img src="${item.user.profile_image.small}" alt="User profile"/>
-            </a>    
-            </div>
+        <div class="visit-btn">
+            <a href=${item.links.html} class="full-link"> Visitar</a>
+        </div>
+        <div class="profile">
+            <img src="${
+              item.user.profile_image.small
+            }" alt="Avatar" class="avatar" style="border: 1px solid ${
+    item.color
+  }"/>
+            <div class="name">${item.user.name}</div>
+            <div class="date">📤 ${item.created_at.split('T')[0]}</div>
         </div>
     </div>
     </li>
