21c8f00221413d217767ce0585ae62f3c378c947
coke_bdj <30370409+cokebdj@users.noreply.github.com>
2024-12-11 13:03:51 +0100

    include pop up

--- 03_entrega/pinterestAsync/main.js
+++ 03_entrega/pinterestAsync/main.js
@@ -20,7 +20,34 @@ const searchPhotos = async (keyword) => {
     perPage: 30
   })
   if (images.response.results.length < 1) {
-    window.alert('No results for the query, but here you have some cats :)')
+    const height = 200
+    const width = 400
+    const screenX =
+      window.screenX !== undefined ? window.screenX : window.screenLeft
+    const screenY =
+      window.screenY !== undefined ? window.screenY : window.screenTop
+    const outerWidth =
+      window.outerWidth !== undefined
+        ? window.outerWidth
+        : document.documentElement.clientWidth
+    const outerHeight =
+      window.outerHeight !== undefined
+        ? window.outerHeight
+        : document.documentElement.clientHeight - 22
+    const left = screenX + (outerWidth - width) / 2
+    const top = screenY + (outerHeight - height) / 2
+    const features = `width=${width},height=${height},left=${left},top=${top},resizable,scrollbars,status`
+
+    const popup = window.open('', 'popupWindow', features)
+    popup.document.write('<html><head><title>No results :(</title></head>')
+    popup.document.write("<body><div class='popup-content'>")
+    popup.document.write(
+      `<p>No results for the query, but here you have some cats :)</p>`
+    )
+    popup.document.write("<button onclick='window.close()'>Close</button>")
+    popup.document.write('</div></body></html>')
+    popup.document.close()
+
     const imagesCats = await unsplash.search.getPhotos({
       query: 'cats',
       page: 1,
